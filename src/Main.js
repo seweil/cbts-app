87826d8b684f1edfb8a1cd01770a485bb7e4011a
Steve Weil <steve.weil@smartsheet.com>
2018-04-10 09:17:59 -0700

    Need to compile scss

--- src/Main.js
+++ src/Main.js
@@ -8,26 +8,32 @@ import {
   import Home from "./Home";
   import Stuff from "./Stuff";
   import Contact from "./Contact";
+  import NavComponent from "./NavBar";
 
 class Main extends Component {
   render() {
-    return (
-        <HashRouter>
+    return <HashRouter>
             <div>
-            <h1>Simple SPA</h1>
-            <ul className="header">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/stuff">Stuff</NavLink></li>
-            <li><NavLink to="/contact">Contact</NavLink></li>
-            </ul>
-            <div className="content">
-                <Route exact path="/" component={Home}/>
-                <Route path="/stuff" component={Stuff}/>
-                <Route path="/contact" component={Contact}/>
+                <NavComponent />
+                 <h1>Simple SPA</h1>
+                <ul className="header">
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/stuff">Stuff</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/contact">Contact</NavLink>
+                    </li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home} />
+                    <Route path="/stuff" component={Stuff} />
+                    <Route path="/contact" component={Contact} />
+                </div>
             </div>
-            </div>
-        </HashRouter>
-    );
+        </HashRouter>;
   }
 }
  
