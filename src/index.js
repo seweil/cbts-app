9cbbdf78714658bfb0098dc7f23cc9a7c0ca7022
Steve Weil <steve.weil@smartsheet.com>
2018-04-14 21:48:57 -0700

    Revert "Basic routing"
    This reverts commit 866918d328d9e2ac40f92299677504582ec21b8d.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+<<<<<<< HEAD
 import './NavBar.css';
 import Main from "./Main";
+=======
+import App from './App';
+>>>>>>> parent of 866918d... Basic routing
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
